5dfccd3577112ee712430ef0893967f442b515e9
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-22 09:37:04 +0200

    modular component

--- devonfw-guide/website/components/header/header.js
+++ devonfw-guide/website/components/header/header.js
@@ -1,12 +1,18 @@
-function loadNavbar(navbarDestSelector, afterLoad = () => {}) {
-  const HTML_FILE = getHtmlFileName();
-  const NAVBAR_SELECTOR = `${HTML_FILE} #content .sect1 .sectionbody ul`;
-  $(navbarDestSelector).load(NAVBAR_SELECTOR, afterLoad);
-}
+const HeaderModule = {
+  loadNavbar: function(navbarDestSelector, afterLoad = () => {}) {
+    const HTML_FILE = this.getHtmlFileName();
+    const NAVBAR_SELECTOR = `${HTML_FILE} #content .sect1 .sectionbody ul`;
+    console.log(HTML_FILE);
+    $(navbarDestSelector).load(NAVBAR_SELECTOR, afterLoad);
+  },
 
-function getHtmlFileName() {
-  let thisFile = $('script[src$="/header.js"]')[0];
-  let thisFilename = thisFile.attributes.src.value;
-  let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
-  return htmlFilemame;
-}
+  getHtmlFileName: function() {
+    console.log('FILENAME');
+    let thisFile = $('script[src$="/header.js"]')[0];
+    let thisFilename = thisFile.attributes.src.value;
+    let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
+    console.log('FILENAME');
+    console.log(htmlFilemame);
+    return htmlFilemame;
+  },
+};
