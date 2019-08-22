fdc753c3914f0aaa2b16f5a4635327e4a0abf292
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-22 10:20:29 +0200

    styled resources css and refactored code

--- devonfw-guide/website/pages/resources/resources.js
+++ devonfw-guide/website/pages/resources/resources.js
@@ -1,13 +1,26 @@
-function loadResources(
-  resourcesHtml,
-  resourcesDestSelector = '#resources-page',
-  handler = () => {},
-) {
-  $(resourcesDestSelector).load(resourcesHtml, function() {
-    let cards = $(resourcesDestSelector + ' [id$="_resources"]')
-      .siblings('.sectionbody')
-      .children('.sect2');
-    $(this).html(cards);
-    handler();
-  });
-}
+(function(window, undefined) {
+  // Function definitions
+  function loadResources(
+    resourcesDestSelector = '#resources-page',
+    handler = () => {},
+  ) {
+    const HTML_FILE = getHtmlFileName();
+    const RESOURCES_SELECTOR = `${HTML_FILE} #content .sect1`;
+
+    $(resourcesDestSelector).load(RESOURCES_SELECTOR, function() {
+      handler();
+    });
+  }
+
+  function getHtmlFileName() {
+    let thisFile = $('script[src$="/resources.js"]')[0];
+    let thisFilename = thisFile.attributes.src.value;
+    let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
+    return htmlFilemame;
+  }
+
+  // List of functions accessibly by other scripts
+  window.ResourcesModule = {
+    loadResources: loadResources,
+  };
+})(window);
