3af35246039a7c378108b2257b0ad49756ed62f2
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-22 10:30:27 +0200

    appendEnd function in header module

--- devonfw-guide/website/components/header/header.js
+++ devonfw-guide/website/components/header/header.js
@@ -1,12 +1,13 @@
-const HeaderModule = {
-  loadNavbar: function(navbarDestSelector, afterLoad = () => {}) {
-    const HTML_FILE = this.getHtmlFileName();
+(function(window, undefined) {
+  // Function definitions
+  function loadNavbar(navbarDestSelector, afterLoad = () => {}) {
+    const HTML_FILE = getHtmlFileName();
     const NAVBAR_SELECTOR = `${HTML_FILE} #content .sect1 .sectionbody ul`;
     console.log(HTML_FILE);
     $(navbarDestSelector).load(NAVBAR_SELECTOR, afterLoad);
-  },
+  }
 
-  getHtmlFileName: function() {
+  function getHtmlFileName() {
     console.log('FILENAME');
     let thisFile = $('script[src$="/header.js"]')[0];
     let thisFilename = thisFile.attributes.src.value;
@@ -14,5 +15,15 @@ const HeaderModule = {
     console.log('FILENAME');
     console.log(htmlFilemame);
     return htmlFilemame;
-  },
-};
+  }
+
+  function appendEnd(navbarDestSelector, element) {
+    $(navbarDestSelector).append(element);
+  }
+
+  // List of functions accessibly by other scripts
+  window.HeaderModule = {
+    loadNavbar: loadNavbar,
+    appendEnd: appendEnd,
+  };
+})(window);
