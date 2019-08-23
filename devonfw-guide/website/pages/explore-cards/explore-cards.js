5315291a7be904af02eb5079494ddaee38e07712
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-23 11:06:22 +0200

    explore cards, some fixes need in the navbar

--- devonfw-guide/website/pages/explore-cards/explore-cards.js
+++ devonfw-guide/website/pages/explore-cards/explore-cards.js
@@ -1,14 +1,35 @@
-function loadExploreCards(
-  cardsHtmlUl,
-  cardDestSelector = '#explore-page',
-  handler = () => {},
-) {
-  $(cardDestSelector).load(cardsHtmlUl, function() {
-    let cards = $(cardDestSelector + ' [id$="_explore-cards"]')
-      .siblings('.sectionbody')
-      .children('.sect2');
-    cards.find('.sect3 .ulist').addClass('website-explore-card');
-    $(this).html(cards);
-    handler();
-  });
-}
+(function(window, undefined) {
+  // Function definitions
+  function loadExploreCards(
+    cardDestSelector = '#explore-page',
+    handler = () => {},
+  ) {
+    const HTML_FILE = getHtmlFileName();
+    const EXPLORE_CARDS_SELECTOR = `${HTML_FILE} #content .sect1`;
+/*
+    $(cardDestSelector).load(cardsHtmlUl, function() {
+      let cards = $(cardDestSelector + ' [id$="_explore-cards"]')
+        .siblings('.sectionbody')
+        .children('.sect2');
+      cards.find('.sect3 .ulist').addClass('website-explore-card');
+      $(this).html(cards);
+      handler();
+    });*/
+
+    $(cardDestSelector).load(EXPLORE_CARDS_SELECTOR, function() {
+      handler();
+    });
+  }
+
+  function getHtmlFileName() {
+    let thisFile = $('script[src$="explore-cards.js"]')[0];
+    let thisFilename = thisFile.attributes.src.value;
+    let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
+    return htmlFilemame;
+  }
+
+  // List of functions accessibly by other scripts
+  window.ExploreModule = {
+    loadExploreCards: loadExploreCards,
+  };
+})(window);
