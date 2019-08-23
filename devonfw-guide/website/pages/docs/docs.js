304d52f6bbca3506ba4ed88087b7627208a67cbf
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-23 13:07:17 +0200

    added basic structure for docs

--- devonfw-guide/website/pages/docs/docs.js
+++ devonfw-guide/website/pages/docs/docs.js
@@ -1,26 +1,23 @@
 (function(window, undefined) {
   // Function definitions
-  function loadCommunity(
-    communityDestSelector = '#explore-page',
-    handler = () => {},
-  ) {
+  function loadDocs(docsDestSelector = '#docs-page', handler = () => {}) {
     const HTML_FILE = getHtmlFileName();
-    const COMMUNITY_SELECTOR = `${HTML_FILE} #content .sect1`;
+    const DOCS_SELECTOR = `${HTML_FILE} #content .sect1`;
 
-    $(communityDestSelector).load(COMMUNITY_SELECTOR, function() {
+    $(docsDestSelector).load(DOCS_SELECTOR, function() {
       handler();
     });
   }
 
   function getHtmlFileName() {
-    let thisFile = $('script[src$="community.js"]')[0];
+    let thisFile = $('script[src$="docs.js"]')[0];
     let thisFilename = thisFile.attributes.src.value;
     let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
     return htmlFilemame;
   }
 
   // List of functions accessibly by other scripts
-  window.CommunityModule = {
-    loadCommunity: loadCommunity,
+  window.DocsModule = {
+    loadDocs: loadDocs,
   };
 })(window);
