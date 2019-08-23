a1dbbe9fd7153d60f2fdd7d8112f4b9a17d0a3a2
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-23 12:12:01 +0200

    community commented cod removed

--- devonfw-guide/website/pages/community/community.js
+++ devonfw-guide/website/pages/community/community.js
@@ -1,11 +1,3 @@
-/*function loadCommunity(communityHtml, communityDestSelector = '#community-page', handler = () => {}) {
-    $(communityDestSelector).load(communityHtml, function() {
-      let cards = $(communityDestSelector + ' [id$="_community"]').siblings('.sectionbody').children('.sect2');
-      $(this).html(cards);
-      handler();
-    });
-}*/
-
 (function(window, undefined) {
   // Function definitions
   function loadCommunity(
