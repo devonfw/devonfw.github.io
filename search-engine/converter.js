c79f50f95b58ad805ccab612311ac49a252aa175
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-27 08:44:56 +0200

    converter formatted

--- search-engine/converter.js
+++ search-engine/converter.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const lunr = require('lunr');
 
-
 function getLunrDoc(dirname, extension) {
   let files = getFilesFromDir(dirname, extension);
 
@@ -16,7 +15,10 @@ function getLunrDoc(dirname, extension) {
 }
 
 function normalize(path) {
-	return path.replace('\\/', '/').replace('//', '/').replace('\\', '/');
+  return path
+    .replace('\\/', '/')
+    .replace('//', '/')
+    .replace('\\', '/');
 }
 
 function getFilesFromDir(dirname, extension) {
@@ -30,9 +32,7 @@ function getFilesFromDir(dirname, extension) {
     fileStats = fs.lstatSync(item);
 
     if (fileStats.isDirectory()) {
-      result = result.concat(
-        getFilesFromDir(item, extension),
-      );
+      result = result.concat(getFilesFromDir(item, extension));
     }
 
     if (fileStats.isFile() && path.extname(item) === extension) {
@@ -53,16 +53,16 @@ function readFromFilename(file) {
   lines.forEach(function(line) {
     let matchRes = line.match(/^=+ (.*)/);
     let lineLevel = occurrences(matchRes ? matchRes[0] : '', '=');
-  
-	if (lineLevel == 0) {
-		matchRes = line.match(/^#+ (.*)/);
-		lineLevel = occurrences(matchRes ? matchRes[0] : '', '#');
-	}
-	
+
+    if (lineLevel == 0) {
+      matchRes = line.match(/^#+ (.*)/);
+      lineLevel = occurrences(matchRes ? matchRes[0] : '', '#');
+    }
+
     let isTitle = lineLevel > 0;
 
-    if (isTitle && titleLevel > lineLevel ) {
-		doc.title = matchRes[1];
+    if (isTitle && titleLevel > lineLevel) {
+      doc.title = matchRes[1];
     }
   });
   return doc;
@@ -73,15 +73,15 @@ function generateIndexJson(documents) {
     this.ref('id');
     this.field('title');
     this.field('body');
-	this.metadataWhitelist = ['position']
+    this.metadataWhitelist = ['position'];
 
     documents.forEach(function(doc) {
       this.add(doc);
     }, this);
   });
-  
+
   let idxJson = JSON.stringify(idx);
-	
+
   fs.writeFileSync('./docs-json.json', JSON.stringify(documents));
   fs.writeFileSync('./index.json', idxJson);
   console.log('The file was saved!');
@@ -91,8 +91,8 @@ function generateIndexJson(documents) {
 
 function query(idxJson) {
   let idx = lunr.Index.load(JSON.parse(idxJson));
-  
-  let query = "installed packet manager start";
+
+  let query = 'installed packet manager start';
   let queryRes = idx.search(query);
   console.log('QUERY RESULT: ');
 }
