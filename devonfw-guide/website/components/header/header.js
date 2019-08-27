074aba18e95cbe7b335ce18a986e7b26dfb1a653
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-27 14:23:53 +0200

    searchbar added everywhere

--- devonfw-guide/website/components/header/header.js
+++ devonfw-guide/website/components/header/header.js
@@ -3,21 +3,39 @@
   function loadNavbar(navbarDestSelector, afterLoad = () => {}) {
     const HTML_FILE = getHtmlFileName();
     const NAVBAR_SELECTOR = `${HTML_FILE} #content .sect1 .sectionbody ul`;
-    console.log(HTML_FILE);
-    $(navbarDestSelector).load(NAVBAR_SELECTOR, afterLoad);
+
+    $(navbarDestSelector).load(NAVBAR_SELECTOR, () => {
+      const searchBar = getSearchBar();
+      HeaderModule.appendEnd(navbarDestSelector, searchBar);
+
+      afterLoad();
+    });
   }
 
   function getHtmlFileName() {
-    let thisFile = $('script[src$="/header.js"]')[0];
+    let thisFile = $('script[src$="header.js"]')[0];
     let thisFilename = thisFile.attributes.src.value;
     let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
     return htmlFilemame;
   }
 
   function appendEnd(navbarDestSelector, element) {
-    console.log('FILENAME');
     const navbarDest = `${navbarDestSelector} > ul:first-child`;
     $(navbarDest).append(element);
+  }
+
+  function getSearchBar() {
+    const searchBar = `
+      <li>
+        <div class="search-bar">
+          <input id="search-field" type="text" placeholder="Search by keyword(s)..."/>
+          <button onclick="query()">Search</button>
+          <div class="search-bar-results hidden" id="search-results">
+          </div>
+        </div>
+      </li>`;
+  
+    return searchBar;
   }
 
   // List of functions accessibly by other scripts
