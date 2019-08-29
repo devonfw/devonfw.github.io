add2aaa07567934ddec81ae83a55234a54dd4d9a
Ambuludi Olmedo <juan-andres.ambuludi-olmedo@capgemini.com>
2019-08-29 13:51:56 +0200

    created logo page

--- devonfw-guide/website/pages/logo/logo.js
+++ devonfw-guide/website/pages/logo/logo.js
@@ -3,7 +3,6 @@ function loadLogo(
   logoDestSelector = '#logo-page',
   handler = () => {},
 ) {
-
   console.info('loading logo page...');
   $(logoDestSelector).load(logoHtmlUl, function() {
     let outmap = $.map(
@@ -32,3 +31,30 @@ function loadLogo(
     console.info('Logo page loaded succesfully!');
   });
 }
+
+(function(window) {
+  // Function definitions
+  function loadLogoPage(logoDestSelector = '#logo-page', handler = () => {}) {
+    const HTML_FILE = getHtmlFileName();
+    const LOGO_SELECTOR = `${HTML_FILE} #content .sect1`;
+
+    console.info('loading logo page...');
+    $(logoDestSelector).load(LOGO_SELECTOR, function() {
+      handler();
+      console.info('Logo page loaded succesfully!');
+    });
+    
+  }
+
+  function getHtmlFileName() {
+    let thisFile = $('script[src$="logo.js"]')[0];
+    let thisFilename = thisFile.attributes.src.value;
+    let htmlFilemame = thisFilename.replace(/\.js$/g, '.html');
+    return htmlFilemame;
+  }
+
+  // List of functions accessibly by other scripts
+  window.LogoModule = {
+    loadLogoPage: loadLogoPage,
+  };
+})(window);
