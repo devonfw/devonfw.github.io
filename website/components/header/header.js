9c722692677cdced8fb27805741caba4232247ee
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2019-10-05 12:12:35 +0200

    fixed header links

--- website/components/header/header.js
+++ website/components/header/header.js
@@ -1,116 +1,6 @@
 import { ConfigModule } from "../../config/devonfw-site-conf.js";
 
 const headerModule = (function(window) {
-  // Function definitions
-  // function loadNavbar(navbarDestSelector, afterLoad = () => {}) {
-  //   const HTML_FILE = getHtmlFileName();
-  //   const NAVBAR_SELECTOR = `${HTML_FILE} .website-navbar`;
-
-  //   $(navbarDestSelector).load(NAVBAR_SELECTOR, () => {
-  //     $(navbarDestSelector).html(navbarTemplate(navbarModel()));
-  //     afterLoad();
-  //   });
-  // }
-
-  // function getHtmlFileName() {
-  //   const componentPath = ConfigModule.componentsLocation.header.path;
-  //   return componentPath;
-  // }
-
-  // function appendEnd(navbarDestSelector, element) {
-  //   const navbarDest = `${navbarDestSelector} > ul:first-child`;
-  //   $(navbarDest).append(element);
-  // }
-
-  // function navbarModel() {
-  //   let model = {
-  //     brand: { text: "", img: "" },
-  //     links: [{ text: "", href: "" }]
-  //   };
-
-  //   let links = [];
-  //   $(".website-navbar li").each(function(index, el) {
-  //     let a = $(el).find("a");
-
-  //     if (index === 0) {
-  //       model.brand.href = a.attr("href");
-  //       model.brand.img = $(el)
-  //         .find("img")
-  //         .attr("src");
-
-  //       console.log(model.brand.img);
-  //     } else {
-  //       let link = {
-  //         text: a.text(),
-  //         href: a.attr("href")
-  //       };
-
-  //       links.push(link);
-  //     }
-  //   });
-  //   model.links = links;
-
-  //   return model;
-  // }
-
-  // function navbarTemplate(navbarModel) {
-  //   function linksTemplate(links) {
-  //     let l = '';
-
-  //     for (let i = 0; i < links.length; i++) {
-  //       l += `
-  //         <li class="nav-item">
-  //           <a class="nav-link text-white" href="${links[i].href}">${links[i].text}</a>
-  //         </li>`;
-  //     }
-
-  //     return l;
-  //   }
-  //   const template = `
-  //     <nav class="navbar navbar-expand-lg navbar-dark bg-custom-blue">
-  //       <button
-  //         class="navbar-toggler"
-  //         type="button"
-  //         data-toggle="collapse"
-  //         data-target="#navbarSupportedContent"
-  //         aria-controls="navbarSupportedContent"
-  //         aria-expanded="false"
-  //         aria-label="Toggle navigation"
-  //       >
-  //         <span class="navbar-toggler-icon"></span>
-  //       </button>
-  //       <a class="navbar-brand text-white mr-auto ml-3 ml-lg-0" href="${
-  //         navbarModel.brand.href
-  //       }">
-  //         <img src="${ConfigModule.editSrc.imgFolderPath}images/Logo_devonfw.svg" width="130" height="30" alt="">
-  //       </a>
-
-  //       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-  //         <ul class="navbar-nav ml-auto">
-  //           ${linksTemplate(navbarModel.links)}
-  //         </ul>
-  //       </div>
-  //       <form class="form-inline my-2 my-lg-0">
-  //           <div class="search-bar">
-  //             <input
-  //               id="search-field"
-  //               type="search"
-  //               class="form-control mr-sm-2"
-  //               placeholder="Search by keyword(s)..."
-  //               aria-label="Search"/>
-  //             <div class="sb-res-pos px-4 z-100">
-  //               <div class="col rounded px-0 border bg-white hidden search-bar-results" id="search-results-box">
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </form>
-  //         <a class="navbar-brand text-white mr-auto ml-3 ml-lg-0" href="https://github.com/devonfw/devonfw-official-website">
-  //           <img src="${ConfigModule.editSrc.imgFolderPath}images/github-mark.png" width=auto height=auto alt="">
-  //         </a>
-  //     </nav>`;
-
-  //   return template;
-  // }
 
   function searchResultTemplate(title, link) {
     let template = `
@@ -193,9 +83,6 @@ const headerModule = (function(window) {
     });
   }
 
-  // List of functions accessibly by other scripts
-  // loadNavbar: loadNavbar,
-  // appendEnd: appendEnd,
   return {
     searchOnClick: searchOnClick,
     queryFunction: query
