945f73e47c400243284fe96d7d9efb69502c1e2f
devonfw-core <devonfw-core@users.noreply.github.com>
2022-06-29 13:04:46 +0000

    Automated deployment: Wed Jun 29 13:04:46 UTC 2022 a0aef5f7573947554bc7f39901f23930deb877e1

--- journeys/scripts/createOutput.sh
+++ journeys/scripts/createOutput.sh
@@ -9,4 +9,30 @@ rm ../target/generated-docs/journeys/journey-pom-template.xml
 rm -r ../target/generated-docs/scripts
 rm -r ../target/generated-docs/website/pages/journeys/journeyData
 
+#node scripts/fixImages.js ../target/generated-docs/journeys
 node scripts/htmlToJson.js ../target/generated-docs/journeys ../target/generated-docs/journeys/journeyData
+
+echo "Start Create Output.sh:"
+echo "../target/generated-docs/journeys:"
+ls ../target/generated-docs/journeys
+printf "\n"
+
+echo "../target/generated-docs/journeys/journeys/journeyData"
+ls ../target/generated-docs/journeys/journeys/journeyData
+printf "\n"
+echo "../target/generated-docs/journeys/journeys/journeyData/index"
+ls ../target/generated-docs/journeys/journeys/journeyData/index
+
+cp -avr devonfw_overview_small.drawio.svg ../target/generated-docs/journeys/journeys/journeyData/index
+
+echo "AFTER Manually COPYING the image: ../target/generated-docs/journeys/journeys/journeyData/index"
+ls ../target/generated-docs/journeys/journeys/journeyData/index
+
+echo "../target/generated-docs/website/pages/journeys/ BEFORE copy of generated-docs/journeys"
+printf "\n"
+ls ../target/generated-docs/website/pages/journeys/
+cp -avr ../target/generated-docs/journeys/ ../target/generated-docs/website/pages/journeys/
+
+echo "../target/generated-docs/website/pages/journeys/ AFTER copy of generated-docs/journeys"
+ls ../target/generated-docs/website/pages/journeys/
+echo "End Create Outpu.sh"
